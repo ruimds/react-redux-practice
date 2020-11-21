b6a85392393ba63cc7a75aab8b29c0eefd839d6c
ruimds <ruidsantos99@gmail.com>
2020-11-21 11:58:33 +0000

    Add ingredient price and functions to handle ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,14 @@
 import React, {Component} from 'react'
 import Aux from '../../hoc/Aux'
 import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+const INGREDIENT_PRICE = {
+    salad: 0.5,
+    cheese: 0.4,
+    bacon: 0.7,
+    meat: 1.3,
+};
 
 class BurgerBuilder extends Component {
     state = {
@@ -9,14 +17,26 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0,
-        }
+        },
+        totalPrice: 4
+    }
+
+    addIngredientHandler = (type) => {
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = this.state.ingredients[type] + 1;;
+        
+        const newPrice = this.state.totalPrice + INGREDIENT_PRICE[type];
+
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
     }
 
     render() {
         return(
             <Aux>
                 <Burger ingredients={this.state.ingredients} /> 
-                <div>Build Controls</div>
+                <BuildControls ingredientAdded = {this.addIngredientHandler}/>
             </Aux>
         );
     }
