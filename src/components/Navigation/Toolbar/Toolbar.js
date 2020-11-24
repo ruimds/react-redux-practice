f3fe1252abc76453f83dbaf3cdf8cfa84d602c74
ruimds <ruidsantos99@gmail.com>
2020-11-24 10:53:06 +0000

    Update toolbar component

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import './Toolbar.css'
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../../Navigation/NavigationItems/NavigationItems'
 
 const toolbar = (props) => (
     <header className="toolbar">
-        <div>MENU</div>
-        <Logo />
-        <nav>
-            <ul>
-
-            </ul>
+        <Logo /> 
+        <nav className="desktop-only">
+            <NavigationItems/>
         </nav>
     </header>
 )
